f268d053fdf00bfe80bdaa5d1cd6cf188d294e8e
Egor Fortov <egorfortov@gmail.com>
2022-10-27 17:30:37 +0300

    Added hrefs, namespace & names

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -6,7 +6,7 @@ from . import views
 app_name = 'posts'
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='main'),
     path('group_list', views.group_list, name='group_list'),
     path('group/<slug:slug>/', views.group_posts, name='group'),
 ]
