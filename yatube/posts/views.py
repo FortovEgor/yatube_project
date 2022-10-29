bbf2096b792483ae948b754b2103007a28403fe2
Egor Fortov <egorfortov@gmail.com>
2022-10-29 22:00:03 +0300

    working version begore task

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -4,20 +4,22 @@ from django.shortcuts import render
 # Create your views here.
 
 
-# ice_cream/views.py
+# posts/views.py
 from django.http import HttpResponse
+from .models import Post
 
 
-# Главная страница
 def index(request):
-    # return HttpResponse('Ты <i>не можешь</i> получить правильные '
-    #                     '<b>ответы</b>,<br> если у тебя нет правильных '
-    #                     '<s>вопросов</s> запросов.')
-    template = 'posts/index.html'
+    # Одна строка вместо тысячи слов на SQL:
+    # в переменную posts будет сохранена выборка из 10 объектов модели Post,
+    # отсортированных по полю pub_date по убыванию (от больших значений
+    # к меньшим)
+    posts = Post.objects.order_by('-pub_date')[:10]
+    # В словаре context отправляем информацию в шаблон
     context = {
-        'title': 'Это главная страница проекта Yatube'
+        'posts': posts,
     }
-    return render(request, template, context)
+    return render(request, 'posts/index.html', context) 
 
 
 # Страница group_list
