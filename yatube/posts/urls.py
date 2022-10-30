7276cce8e2299d2e51482a1453a88ffc28b4050b
Egor Fortov <egorfortov@gmail.com>
2022-10-30 18:11:58 +0300

    working before final assessment

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -8,5 +8,5 @@ app_name = 'posts'
 urlpatterns = [
     path('', views.index, name='main'),
     path('group_list', views.group_list, name='group_list'),
-    path('group/<slug:slug>/', views.group_posts, name='group'),
+    path('group/<slug:slug>/', views.group_posts, name='group_list'),
 ]
